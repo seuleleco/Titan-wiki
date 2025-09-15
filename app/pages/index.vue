8813e9aad510cf89287521feb2ac220a97d3c810
ILHA SERVICE - Leonardo Pereira Pereira <leonardo.pereira.3@ebserh.gov.br>
2025-09-14 21:25:54 -0300

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    teste
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+  </div>
+</template>
+
+<script setup>
+import "bootstrap/dist/css/bootstrap.min.css";
+</script>
